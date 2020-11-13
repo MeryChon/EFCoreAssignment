581f00831e55911f1c583eea6ecf58addd844301
MeriChon <meri@fina2.net>
2020-11-13 11:51:45 +0400

    Create MS test project; Add first test;

---
+++ EFCoreAssignment/EFCoreAssignement.Test/ScoresFileReaderTest.cs
@@ -0,0 +1,28 @@
+﻿using EFCoreAssignment.Util;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace EFCoreAssignement.Test
+{
+    [TestClass]
+    class ScoresFileReaderTest
+    {
+        private readonly string ProjectDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+
+        [TestMethod]
+        public void TestReadFileLinesOk()
+        {
+            string filePath = ProjectDirectory + @"Test\Resources\StudentPointsDataOk.csv";
+            //UniversityDbContext dbContext = new UniversityDbContext();
+            //IUniversityApi api = new UniversityApi(dbContext);
+            ScoresFileReader sfr = new ScoresFileReader();
+
+            string[] lines = sfr.ReadLines(filePath);
+            Assert.IsTrue(lines.Length == 4);
+            Assert.AreEqual(lines[0], "StudentId,SubjectId,Point");
+            Assert.AreEqual(lines[1], "10,3,90");
+            Assert.AreEqual(lines[2], "2,4,67");
+            Assert.AreEqual(lines[3], "15,2,51");
+        }
+    }
+}
---
+++ EFCoreAssignment/EFCoreAssignement.Test/UniversityApiTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EFCoreAssignement.Test
+{
+    [TestClass]
+    public class UniversityApiTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
